aa5e7e6624e53d44bd9cb5af1d948ef29c8f49fa
Aditya Vikram KIrtania <108626210+Aditya792max@users.noreply.github.com>
2026-08-25 13:18:27 +0530

    Added Tickets

---
+++ Server/Models/CommentModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+
+    text: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Comment = mongoose.model("Comment", CommentSchema);
+
+module.exports = Comment;
